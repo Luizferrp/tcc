e6e9fce814517db96240ff20e00bbf5a602c5555
Luizferrp <luizreis.p@icloud.com>
2025-03-30 09:24:41 -0300

    setting setup init script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DOWNLOADDIR=rawdata
+mkdir -p $DOWNLOADDIR
+
+if [ ! -d $DOWNLOADDIR ]; then 
+  echo 'error creating the rawdata folder';
+  exit 1;
+fi
+
+curl -L -o $DOWNLOADDIR/nslkdd.zip  https://www.kaggle.com/api/v1/datasets/download/hassan06/nslkdd
+curl -L -o $DOWNLOADDIR/bot-iot.zip  https://www.kaggle.com/api/v1/datasets/download/vigneshvenkateswaran/bot-iot
+curl -L -o $DOWNLOADDIR/cicidscollection.zip https://www.kaggle.com/api/v1/datasets/download/dhoogla/cicidscollection
